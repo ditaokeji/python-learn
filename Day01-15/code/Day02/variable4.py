8c5458a8f097936d877ed338e15ff278817f4da1
冯振威 <1181399414@qq.com>
2023-08-18 17:36:59 +0800

    feat: day2

---
+++ Day01-15/code/Day02/variable4.py
@@ -0,0 +1,18 @@
+a = 0
+b = bool(a)
+c = ''
+d = bool(c)
+e = False
+f = int(e)
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+print(c)
+print(type(c))
+print(d)
+print(type(d))
+print(e)
+print(type(e))
+print(f)
+print(type(f))
