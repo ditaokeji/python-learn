9d1dec209b9e5274030b13bd314c61cedf9071be
冯振威 <1181399414@qq.com>
2023-08-18 15:14:26 +0800

    feat: day01

---
+++ Day01-15/code/Day01/turtle-demo2.py
@@ -0,0 +1,18 @@
+from turtle import Screen, Turtle
+
+screen = Screen()
+
+turtle = Turtle()
+
+turtle.pensize(4)
+turtle.pencolor('red')
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+screen.mainloop()
