e18153f1c01e81794f82b1b0c55c6f37ac20f933
冯振威 <1181399414@qq.com>
2023-09-14 23:40:51 +0800

    feat: day08

---
+++ Day01-15/code/Day08/student.py
@@ -0,0 +1,27 @@
+def _foo():
+    print('test')
+
+class Student(object):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def study(self, course_name):
+        print('%s正在学习%s.' % (self.name, course_name))
+
+    def watch_av(self):
+        if self.age < 18:
+            print('%s只能观看《熊出没》.' % self.name)
+        else:
+            print('%s正在观看岛国大电影.' % self.name)
+
+def main():
+    stu1 = Student('feng', 18)
+    stu1.study('Python程序设计')
+    stu1.watch_av()
+    stu2 = Student('王大锤', 15)
+    stu2.study('思想品德')
+    stu2.watch_av()
+
+if __name__ == '__main__':
+    main()
