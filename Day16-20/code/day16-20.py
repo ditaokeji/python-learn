0a8b7c8d6d3e551e9999c908946f6cfdf227f692
黄艺 <huangyi@tungee.com>
2023-09-14 09:16:00 +0800

    fix: day16

--- Day16-20/code/day16-20.py
+++ Day16-20/code/day16-20.py
@@ -1,11 +1,3 @@
-'''
-Author: 黄艺 huangyi@tungee.com
-Date: 2023-08-27 22:05:37
-LastEditors: 黄艺 huangyi@tungee.com
-LastEditTime: 2023-08-27 22:33:08
-FilePath: \python-learn\Day16-20\code\day16-20.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
 #生成式的用法
 
 prices = {
@@ -27,4 +19,135 @@ print(prices3)
 names = ['关羽', '张飞', '赵云', '马超', '黄忠']
 courses = ['语文', '数学', '英语']
 
-source = [(name, course) for name in names for course in courses]
+# source = [(name, course) for name in names for course in courses]
+source = [[None] * len(courses) for _ in range(len(names))] 
+print(source)
+
+# for row, name in enumerate(names):
+#     for col, course in enumerate(courses):
+#         scores[row][col] = float(input(f'请输入{name}的{course}成绩: '))
+#         print(scores)
+        
+        
+# heapq模块（堆排序）
+import heapq
+
+list1 = [34, 25, 12, 99, 87, 63, 58, 78, 88, 92]
+list2 = [
+    {'name': 'IBM', 'shares': 100, 'price': 91.1},
+    {'name': 'AAPL', 'shares': 50, 'price': 543.22},
+    {'name': 'FB', 'shares': 200, 'price': 21.09},
+    {'name': 'HPQ', 'shares': 35, 'price': 31.75},
+    {'name': 'YHOO', 'shares': 45, 'price': 16.35},
+    {'name': 'ACME', 'shares': 75, 'price': 115.65}
+]
+
+print(heapq.nlargest(3, list1))
+print(heapq.nsmallest(3, list1))
+print(heapq.nlargest(2, list2, key=lambda x: x['price']))
+print(heapq.nlargest(2, list2, key=lambda x: x['shares']))
+
+
+import itertools
+
+itertools.permutations('ABCD')
+itertools.combinations('ABCDE', 3)
+itertools.product('ABCD', '123')
+itertools.cycle('ABCD')
+
+# collections模块
+from collections import Counter
+
+words = [
+    'look', 'into', 'my', 'eyes', 'look', 'into', 'my', 'eyes',
+    'the', 'the', 'the', 'the', 'the', 'the',
+    'not', 'around', 'the', 'eyes', "don't", 'look', 'around', 'the',
+    'eyes', 'look', 'into', 'my', 'eyes', "you're", 'under'
+]
+
+counter = Counter(words)
+
+# 排序算法
+def select_sort(origin_items, comp=lambda x, y: x < y):
+    """简单选择排序"""
+    items = origin_items[:]
+    for i in range(len(items) - 1):
+        min_index = i
+        for j in range(i + 1, len(items)):
+            if comp(items[j], items[min_index]):
+                min_index = j
+        items[i], items[min_index] = items[min_index], items[i]
+    return items
+ 
+def bubble_sort(origin_items, comp=lambda x, y: x > y):
+    """高质量冒泡排序(搅拌排序)"""
+    items = origin_items[:]
+    for i in range(len(items) - 1):
+        swapped = False
+        for j in range(i, len(items) - 1 - i):
+            if comp(items[j], items[j + 1]):
+                items[j], items[j + 1] = items[j + 1], items[j]
+                swapped = True
+        if swapped:
+            swapped = False
+            for j in range(len(items) - 2 - i, i, -1):
+                if comp(items[j - 1], items[j]):
+                    items[j], items[j - 1] = items[j - 1], items[j]
+                    swapped = True
+        if not swapped:
+            break
+    return items  
+
+def merge(items1, items2, comp=lambda x, y: x <= y):
+    """合并(将两个有序的列表合并成一个有序的列表)"""
+    items = []
+    index1, index2 = 0, 0
+    while index1 < len(items1) and index2 < len(items2):
+        if comp(items1[index1], items2[index2]):
+            items.append(items1[index1])
+            index1 += 1
+        else:
+            items.append(items2[index2])
+            index2 += 1
+    items += items1[index1:]
+    items += items2[index2:]
+    return items
+
+def merge_sort(items, comp=lambda x, y: x <= y):
+    """归并排序(分治法)"""
+    if len(items) < 2:
+        return items[:]
+    mid = len(items) // 2
+    left = merge_sort(items[:mid], comp)
+    right = merge_sort(items[mid:], comp)
+    return merge(left, right, comp)
+  
+def seq_search(items, key):
+    """顺序查找"""
+    for index, item in enumerate(items):
+        if item == key:
+            return index
+    return -1 
+
+def bin_search(items, key):
+    """折半查找"""
+    start, end = 0, len(items) - 1
+    while start <= end:
+        mid = (start + end) // 2
+        if key > items[mid]:
+            start = mid + 1
+        elif key < items[mid]:
+            end = mid - 1
+        else:
+            return mid
+    return -1 
+
+# 公鸡5元一只 母鸡3元一只 小鸡1元三只
+# 用100元买100只鸡 问公鸡/母鸡/小鸡各多少只
+for x in range(20):
+    for y in range(33):
+        z = 100 - x - y
+        if 5*x + 3*y + z/3 == 100:
+            print(x, y, z)
+            
+    
