36ad3b1ab5ad83b00182cc646fbe8b2ccf98241b
黄艺 <huangyi@tungee.com>
2023-10-10 23:53:17 +0800

    fix: review day41

--- Day41-55/day41/hellodjango/hellodjango/urls.py
+++ Day41-55/day41/hellodjango/hellodjango/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from frist.views import show_index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', show_index)
 ]
