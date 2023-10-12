059517bc2dc6109190d43be955601e2acccd4452
冯振威 <1181399414@qq.com>
2023-10-12 23:51:27 +0800

    feat: day13

---
+++ Day01-15/code/Day13/asyncio1.py
@@ -0,0 +1,14 @@
+import asyncio
+import threading
+
+@asyncio.coroutine
+def hello():
+    print('%s: hello, world!' %threading.current_thread())
+    yield from asyncio.sleep(2)
+    print('%s: goodbye, world!' %threading.current_thread())
+
+loop = asyncio.get_event_loop()
+tasks = [hello(), hello()]
+loop.run_until_complete(asyncio.wait(tasks))
+print('game over')
+loop.close()
